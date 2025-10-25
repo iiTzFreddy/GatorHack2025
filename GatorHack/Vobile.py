55af7d384c3ff424d8c5e065098fbdabddde7a28
iiTzFreddy <freddyrives001@gmail.com>
2025-10-24 22:20:48 -0400

    averge genai test

--- GatorHack/Vobile.py
+++ GatorHack/Vobile.py
@@ -1,9 +1,9 @@
-print("Welcome to Gator Hack")
-print("TEST")
-print("mikey")
-print("pedro")
-for i in range(4):
-    print (2)
+from google import genai
+client = genai.Client(api_key="AIzaSyBVzUjYRJj5URDaFHxT2jpa7ZGm7IUiYFE")
+nums = [1,2,3,4,5,6]
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents= f"Only give the average of {nums}"
+)
 
-print("new changes")
-print("freddy's changes")
+print(response.text)
